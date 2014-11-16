31c156280e4e3e6ee25ec27b592ab2db866b24a6
ras9841 <ras9841@rit.edu>
2014-11-16 14:17:56 -0500

    Update Tortuga.py

--- Tortuga.py
+++ Tortuga.py
@@ -11,4 +11,91 @@ def Tortuga():
 def back():
   pass
 
-def begin_fill():
+def begin_poly():
+  pass
+
+def circle():
+  pass
+
+def degrees():
+  pass
+
+def down():
+  pass
+
+def end_poly():
+  pass
+
+def fd():
+  pass
+
+def get_poly():
+  pass
+
+def get_shapepoly():
+  pass
+
+def get_pen():
+  pass
+
+def heading():
+  pass
+
+def is_down():
+  pass
+
+def left():
+  pass
+
+def pen_up():
+  pass
+
+def radians():
+  pass
+
+def right():
+  pass
+
+def set_h():
+  pass
+
+def set_tiltangle():
+  pass
+
+def set_undo_buffer():
+  pass
+
+def shape():
+  pass
+
+def shape_size():
+  pass
+
+def shape_transform():
+  pass
+
+def shear_factor():
+  pass
+
+def speed():
+  pass
+
+def stamp():
+  pass
+
+def tilt():
+  pass
+
+def tiltangle():
+  pass
+
+def towards():
+  // Not implemented
+  pass
+
+def turtle():
+  pass
+def undo():
+    // Not implemented
+
+def
