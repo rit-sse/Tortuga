e90c2b002ae21518627ade9062ae53f380b3d030
ras9841 <ras9841@rit.edu>
2014-11-16 14:39:00 -0500

    finished stubs

--- Tortuga.py
+++ Tortuga.py
@@ -13,69 +13,195 @@ def begin_fill():
   pass
 
 def begin_poly():
+  // Already implemented
   pass
 
 def circle():
   pass
 
+def clear():
+  // Not implemented
+  pass
+
+def clearstamps():
+  // Not implemented
+  pass
+  
+def clone():
+  // Not implemented
+  pass
+
+def color():
+  // Not implemented
+  pass
+
+def currentLine():
+  // Not implemented
+  pass
+
+def currentLineItem():
+  // Not implemented
+  pass
+
 def degrees():
+  pass
+
+def distance():
+  // Not implemented
+  pass
+
+def dot():
+  // Not implemented
   pass
 
 def down():
   pass
 
+def end_fill():
+  // Not implemented
+  pass
+
 def end_poly():
+  // Already implemented
   pass
 
 def fd():
   pass
 
+def forward():
+  pass
+
+def fillcolor():
+  // Not implemented
+  pass
+
+def filling():
+  // Not implemented
+  pass
+
 def get_poly():
+  // Already implemented
   pass
 
 def get_shapepoly():
+  // Already implmented
   pass
 
-def get_pen():
+def getpen():
+  // Already implemented
+  pass
+
+def getscreen():
+  // Not implemented
+  pass
+
+def goto():
+  // Not implemented
   pass
 
 def heading():
+  // Already implemented
   pass
 
-def is_down():
+def hideturtle():
+  // Not implemented
+  pass
+
+def home():
+  // Not implemented
+  pass
+
+def isdown():
+  // Already implemented
+  pass
+
+def isvisible():
+  // Not implemented
   pass
 
 def left():
   pass
 
-def pen_up():
+def onclick():
+  // Not implemented
+  pass
+
+def ondrag():
+  // Not implemented
+  pass
+
+def onrelease():
+  // Not implemented
+  pass
+
+def pen():
+  // Not implemented
+  pass
+
+def pencolor():
+  // Not implemented
+  pass
+
+def pensize():
+  // Not implemented
+  pass
+
+def penup():
+  pass
+
+def pos():
+  // Not implemented
   pass
 
 def radians():
   pass
 
+def reset():
+  // Not implemented
+  pass
+
+def resizemode():
+  // Not implemented
+  pass
+
 def right():
   pass
 
-def set_h():
+def screen():
+  // Not implemented
   pass
 
-def set_tiltangle():
+def seth():
   pass
 
-def set_undo_buffer():
+def settiltangle():
+  pass
+
+def setundobuffer():
+  pass
+
+def setx():
+  // Not implemented
+  pass
+
+def sety():
+  // Not implemented
   pass
 
 def shape():
   pass
 
-def shape_size():
+def shapesize():
   pass
 
-def shape_transform():
+def shapetransform():
   pass
 
-def shear_factor():
+def shearfactor():
+  pass
+
+def showturtle():
+  // Not implemented
   pass
 
 def speed():
